9d4555e0e752e5fa35cefc41fa2e1ecb3af22941
luiz.celeghin <luiz.celeghin@accenture.com>
2025-08-08 18:30:32 -0300

    github actions

--- Java/src/main/java/HelloWorld.java
+++ Java/src/main/java/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello from Java CI/CD Pipeline!");
+        while (true) {
+            System.out.println("Still running...");
+            Thread.sleep(30000);
+        }
     }
 }
