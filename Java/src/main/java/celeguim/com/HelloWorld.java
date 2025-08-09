bcd2763a832affa0e14b36fb172fbbb5c9f47d96
Luiz Celeghin <luiz.celeguim@gmail.com>
2025-08-09 14:19:21 -0300

    gitops updates

--- Java/src/main/java/celeguim/com/HelloWorld.java
+++ Java/src/main/java/celeguim/com/HelloWorld.java
@@ -1,9 +1,14 @@
+package celeguim.com;
+
 public class HelloWorld {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello from Java CI/CD Pipeline!");
+        
         while (true) {
+            String agora = new java.util.Date().toString();
+        	System.out.println("\n"+ agora);
             System.out.println("Still running...");
-            Thread.sleep(30000);
+            Thread.sleep(15000);
         }
     }
 }
