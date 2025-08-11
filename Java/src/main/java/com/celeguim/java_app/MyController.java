517d88efcccdac9792490dedf84424a0b771ddd0
Luiz Celeghin <luiz.celeguim@gmail.com>
2025-08-10 21:33:39 -0300

    git actions update

--- Java/src/main/java/com/celeguim/java_app/MyController.java
+++ Java/src/main/java/com/celeguim/java_app/MyController.java
@@ -2,13 +2,15 @@ package com.celeguim.java_app;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
 
 @RestController
-public class MyController {
+public class MyController  {
 
     @GetMapping("/")
-    public String index() {
+    public String index() throws java.net.UnknownHostException {
+        String hostname = InetAddress.getLocalHost().getHostName();
         String agora = new java.util.Date().toString();
-        return agora + "<br>Hello World from SpringBoot";
+        return agora + "<br>" + hostname + "<br>Hello World from SpringBoot";
     }
 }
